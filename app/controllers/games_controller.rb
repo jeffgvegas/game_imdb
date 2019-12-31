f04b1bf7a45b08c3633b864deedebcd44053c835
Jeffery Gontarek <jeffery@jeffsmac.local>
2019-12-30 20:11:16 -0800

    games_controller updates

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,4 +6,17 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def edit
+    @game =- Game.find(params[:id])
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to(:game_path)
 end
