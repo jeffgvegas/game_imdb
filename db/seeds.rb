a0db2fa0d73f440ec022dcccd9174780581c5b04
Jeffery Gontarek <jeffery@jeffsmac.local>
2020-01-03 21:25:54 -0800

    Brain hurts frustration setting in

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,10 @@
 require "faker"
-require "faker-game"
-100.times do 
-  Game.create(
-    title: Faker::Game.name
-    release_date: Faker::Date
-    rating: %w(0 1 2 3 4 5 6 7 8 9)
+100.times do |number|
+  Game.create({
+    title: Faker::Game.name,
+    release_date: Faker::Date,
+    rating: %w(0 1 2 3 4 5 6 7 8 9),
     console: %w(NES Sega XBOX Playstation)
-  )
+  })
 end
-  title, release date, rating, and console
+  
